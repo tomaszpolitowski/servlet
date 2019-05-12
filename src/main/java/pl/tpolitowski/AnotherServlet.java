09004acaa410466f9864045245c8586e0e3a7bb3
Tomasz Politowski <tomasz.politowski.tp@gmail.com>
2019-05-12 09:07:47 +0200

    day2

--- src/main/java/pl/tpolitowski/AnotherServlet.java
+++ src/main/java/pl/tpolitowski/AnotherServlet.java
@@ -1,11 +1,13 @@
 package pl.tpolitowski;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 
 public class AnotherServlet extends HttpServlet {
@@ -14,11 +16,24 @@ public class AnotherServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out =resp.getWriter();
         out.println("Hello World from Another Servlet");
-        String name = req.getParameter("name");
 
-        out.println("Hello World from mvnll " + name );
+
+//        Cookie[] cookies = req.getCookies();
+////        for (int i = 0; i <cookies.length ; i++) {
+////            if (cookies[i].getName().equals("cookieName"))
+////            out.println(cookies[i].getValue());
+//        String name = Arrays.stream(req.getCookies())
+//                .filter(cookie -> cookie.getName().equals("cookieName"))
+//                .findAny().get().getValue();
+
+      //out.println(name);
+      out.println(req.getSession().getAttribute("name"));
+      out.println(req.getSession().getAttribute("password"));
+
+        }
+
 
     }
 
 
-}
+
--- src/main/java/pl/tpolitowski/MyServlet.java
+++ src/main/java/pl/tpolitowski/MyServlet.java
@@ -2,6 +2,7 @@ package pl.tpolitowski;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,11 +19,23 @@ public class MyServlet extends HttpServlet {
         //resp.sendRedirect(req.getContextPath()+"/second");
         //getServletContext().getRequestDispatcher("/second").forward(req,resp);
        // getServletContext().getRequestDispatcher("/second").include(req,resp);
+
+
+       // Cookie cookie = new Cookie("cookieName", name);
+        //resp.addCookie(cookie);
     }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("Hello from postman");
+        //out.println("Hello from postman");
+        String name = req.getParameter("name");
+        String password = req. getParameter("password");
+
+       // out.println( name + password );
+        req.getSession().setAttribute("name", name);
+        req.getSession().setAttribute("password",password);
+        out.println("<a href =\"/second\"> click</a>");
     }
 }
